e4c591518bb7e7c729987b86aa66db03f8b4218d
Déspina Platakidou <depl22@student.bth.se>
2023-04-03 15:14:19 +0200

    Initial commit

---
+++ src/Controller/QuoteController.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuoteController {
+    #[Route("/api/quote", name: "quote")]
+    public function jsonQuote(): Response
+    {
+        #Lista med citat
+        $quotes = [
+            'Ett glatt hjärta får ansiktet att lysa upp, men hjärtesorg slår ner modet.',
+            'För den betryckte är alla dagar mörka, men för den som är väl till mods är varje dag en fest.',
+            'Glad blir den som ger ett bra svar, ja, vad är bättre än ett ord i rätt tid!',
+            'Ett mjukt svar stillar raseri, men hårda ord väcker vrede.',
+            'De visa använder sin tunga med kunskap, men ur den oförståndiges mun flödar dumheter.'
+        ];
+
+        #Slumpmässigt välja ett citat
+        $randomQuote = $quotes[array_rand($quotes)];
+
+        #Göra en array med citat, dagens datum, tidstämpel för när svaret genererades
+        $data = [
+            'quote' => $randomQuote,
+            'date' => date('Y-m-d'),
+            'timestamp' => time()
+        ];
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+                    $response->getEncodingOptions() | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE
+                );
+                return $response;
+            }
+}
---
+++ src/Controller/ReportControllerTwig.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ReportControllerTwig extends AbstractController
+{
+    // 2.1 Skapa en route / som ger en presentation av dig själv inklusive en bild.
+    #[Route("/", name: "me")]
+    public function me(): Response
+    {
+        return $this->render('me.html.twig');
+    }
+
+    // 2.2 Skapa en route /about som berättar om kursen mvc och dess syfte.
+    #[Route("/about", name: "about")]
+    public function about(): Response
+    {
+        return $this->render('about.html.twig');
+    }
+
+    // 2.3 Skapa en route /report där du samlar dina redovisningstexter för kursens kmom.
+    #[Route("/report", name: "report")]
+    public function report(): Response
+    {
+        return $this->render('report.html.twig');
+    }
+}
