e76019ecbaced8adfbd39b5d28593a1dc4dc7849
Déspina Platakidou <depl22@student.bth.se>
2023-06-02 16:23:17 +0200

    Added method to decide winner for project

--- src/Card/Score.php
+++ src/Card/Score.php
@@ -18,28 +18,29 @@ class Score
     public function calculateHandValue(string $cardsList): int
     {
         preg_match_all('/\[(.*?)\]/', $cardsList, $matches);
-        $values = $matches[1];
+    $values = $matches[1];
 
-        $sum = 0;
-        foreach ($values as $value) {
-            $value = substr($value, 0, 1);
-            if ($value == 'J') {
-                $sum += 11;
-                continue;
-            } elseif ($value == 'Q') {
-                $sum += 12;
-                continue;
-            } elseif ($value == 'K') {
-                $sum += 13;
-                continue;
-            } elseif ($value == 'A') {
-                $sum += 1;
-                continue;
-            }
-            $sum += (int)$value;
+    $sum = 0;
+    foreach ($values as $value) {
+        $cardValue = substr($value, 0, -3);
+
+        if ($cardValue == 'J') {
+            $sum += 11;
+        } elseif ($cardValue == 'Q') {
+            $sum += 12;
+        } elseif ($cardValue == 'K') {
+            $sum += 13;
+        } elseif ($cardValue == 'A') {
+            $sum += 1;
+        } elseif ($cardValue == '10') {
+            $sum += 10;
+        } else {
+            $sum += (int)$cardValue;
         }
-        return $sum;
     }
+
+    return $sum;
+}
 
     /**
      * The method compares the scores of the player and the bankir,
@@ -71,4 +72,47 @@ class Score
         }
         return "Bankiren vinner med totalt värde $bankirScore";
     }
+
+    /**
+     * The method compares the scores of the player and the bankir,
+     * and returns a message indicating who is the winner.
+     *
+     * @param array $playerScores The score of the players hand.
+     * @param int $bankirScore The score of the bankir's hand.
+     * @return string The result message indicating the winner.
+     */
+    public function projectCompareHands(array $playerScores, int $bankirScore): string
+    {
+        $winningHandIndex = 1;
+        $highestScore = $playerScores[0];
+        $isDraw = false;
+
+        for ($i = 1; $i < count($playerScores); $i++) {
+            $playerScore = $playerScores[$i];
+
+            if ($playerScore > $highestScore && $playerScore <= 21) {
+                // Hittat en ny högsta poäng
+                $winningHandIndex = $i;
+                $highestScore = $playerScore;
+                $isDraw = false;
+            } elseif ($playerScore == $highestScore && $playerScore <= 21) {
+                // Oavgjort mellan flera händer
+                $isDraw = true;
+            }
+        }
+
+        if ($bankirScore > 21) {
+            // Bankiren förlorar om poängen överstiger 21
+            return "Spelaren vinner med hand$winningHandIndex och totalt värde $highestScore.";
+        }
+
+        if ($isDraw) {
+            // Oavgjort mellan flera händer
+            return "Det är oavgjort mellan flera händer med totalt värde $highestScore.";
+        }
+
+        // Bankiren vinner om ingen annan vinnare hittades
+        return "Bankiren vinner hand$winningHandIndex med totalt värde $bankirScore.";
+    }
+
 }
